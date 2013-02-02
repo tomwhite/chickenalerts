428d6c29a4a64e03d418aaa9f73817edbe6c7a42
Tom White <tom.e.white@gmail.com>
2013-02-02 17:24:43 +0000

    First working version.

---
+++ src/com/tom_e_white/chickenalerts/ChickenAlertReceiver.java
@@ -0,0 +1,23 @@
+package com.tom_e_white.chickenalerts;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class ChickenAlertReceiver extends BroadcastReceiver {
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Notification notification = new Notification.Builder(context)
+				.setContentTitle("Chicken Alert")
+				.setContentText("Have you put the chickens to bed?")
+				.setTicker("Have you put the chickens to bed?")
+				.setSmallIcon(R.drawable.ic_launcher).build();
+		notification.flags |= Notification.FLAG_AUTO_CANCEL;
+		NotificationManager notificationManager = (NotificationManager) context
+				.getSystemService(Context.NOTIFICATION_SERVICE);
+		notificationManager.notify(0, notification);
+	}
+
+}
---
+++ src/com/tom_e_white/chickenalerts/MainActivity.java
@@ -0,0 +1,38 @@
+package com.tom_e_white.chickenalerts;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+	public void startAlert(View view) {
+		Intent intent = new Intent(this, ChickenAlertReceiver.class);
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(
+				this.getApplicationContext(), 234324243, intent, 0);
+		AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+		alarmManager.set(AlarmManager.RTC_WAKEUP,
+				System.currentTimeMillis() + 5000, pendingIntent);
+		Toast.makeText(this, "Chicken Alarm set for 5 seconds",
+				Toast.LENGTH_LONG).show();
+	}
+
+}
