1be9819948a4512df2fcc706aa50c763c4155574
Tom White <tom.e.white@gmail.com>
2013-02-03 09:49:19 +0000

    Use notification to play sound.

--- src/com/tom_e_white/chickenalerts/ChickenAlertReceiver.java
+++ src/com/tom_e_white/chickenalerts/ChickenAlertReceiver.java
@@ -5,31 +5,24 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.media.MediaPlayer;
-import android.media.MediaPlayer.OnCompletionListener;
+import android.net.Uri;
 
 public class ChickenAlertReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		Uri sound = Uri.parse("android.resource://" + context.getPackageName() + "/" + R.raw.cluck);
 		Notification notification = new Notification.Builder(context)
 				.setContentTitle("Chicken Alert")
 				.setContentText("Have you put the chickens to bed?")
 				.setTicker("Have you put the chickens to bed?")
+				.setSound(sound)
 				.setSmallIcon(R.drawable.ic_launcher).build();
 		notification.flags |= Notification.FLAG_AUTO_CANCEL;
 		NotificationManager notificationManager = (NotificationManager) context
 				.getSystemService(Context.NOTIFICATION_SERVICE);
 		notificationManager.notify(0, notification);
 		
-		MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.cluck);
-		mediaPlayer.start();
-		mediaPlayer.setOnCompletionListener(new OnCompletionListener() {
-	        @Override
-	        public void onCompletion(MediaPlayer mp) {
-	            mp.release();                
-	        }
-	    });
-		
+		// Schedule next alert
 		AlertScheduler scheduler = new AlertScheduler();
 		scheduler.scheduleNextAlert(context);
 	}
