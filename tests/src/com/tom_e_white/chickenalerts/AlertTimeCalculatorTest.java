aa58eab51045dd2a71cade28a7811f1ca3cae673
Tom White <tom.e.white@gmail.com>
2013-02-03 14:55:47 +0000

    Add unit test.

---
+++ tests/src/com/tom_e_white/chickenalerts/AlertTimeCalculatorTest.java
@@ -0,0 +1,47 @@
+package com.tom_e_white.chickenalerts;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import junit.framework.TestCase;
+
+public class AlertTimeCalculatorTest extends TestCase {
+	
+	public void testToday() {
+		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+		cal.set(Calendar.YEAR, 2013);
+		cal.set(Calendar.MONTH, Calendar.FEBRUARY);
+		cal.set(Calendar.DATE, 3);
+		cal.set(Calendar.HOUR_OF_DAY, 12);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		
+		AlertTimeCalculator calculator = new AlertTimeCalculator();
+		Calendar nextAlert = calculator.calculateNextAlert(cal);
+		
+		assertEquals(nextAlert.get(Calendar.YEAR), 2013);
+		assertEquals(nextAlert.get(Calendar.MONTH), Calendar.FEBRUARY);
+		assertEquals(nextAlert.get(Calendar.DATE), 3);
+		assertEquals(nextAlert.get(Calendar.HOUR_OF_DAY), 17);
+		assertEquals(nextAlert.get(Calendar.MINUTE), 49);
+	}
+
+	public void testTomorrow() {
+		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+		cal.set(Calendar.YEAR, 2013);
+		cal.set(Calendar.MONTH, Calendar.FEBRUARY);
+		cal.set(Calendar.DATE, 3);
+		cal.set(Calendar.HOUR_OF_DAY, 18);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		
+		AlertTimeCalculator calculator = new AlertTimeCalculator();
+		Calendar nextAlert = calculator.calculateNextAlert(cal);
+		
+		assertEquals(nextAlert.get(Calendar.YEAR), 2013);
+		assertEquals(nextAlert.get(Calendar.MONTH), Calendar.FEBRUARY);
+		assertEquals(nextAlert.get(Calendar.DATE), 4); // next day
+		assertEquals(nextAlert.get(Calendar.HOUR_OF_DAY), 17);
+		assertEquals(nextAlert.get(Calendar.MINUTE), 51); // sunset is 2 mins later
+	}
+}
