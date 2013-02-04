28e0ae56e7357fce06f184e5c90f6f95cfa721e7
Tom White <tom.e.white@gmail.com>
2013-02-04 22:05:15 +0000

    Use a preference fragment for the enabled switch.

--- src/com/tom_e_white/chickenalerts/MainActivity.java
+++ src/com/tom_e_white/chickenalerts/MainActivity.java
@@ -8,12 +8,13 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import android.view.Menu;
+import android.preference.Preference;
 import android.view.View;
 import android.widget.Toast;
-import android.widget.ToggleButton;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SharedPreferences.OnSharedPreferenceChangeListener {
+	
+	private SettingsFragment fragment;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,18 +23,17 @@ public class MainActivity extends Activity {
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
+	protected void onResume() {
+		super.onResume();
+		fragment = (SettingsFragment) getFragmentManager().findFragmentById(R.id.frag);
+		fragment.getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
 	}
 	
-	public void onToggleClicked(View view) {
-	    boolean on = ((ToggleButton) view).isChecked();
-	    if (on) {
-	    	enableAlerts();
-	    } else {
-	    	disableAlerts();
-	    }
+	@Override
+	protected void onPause() {
+		super.onPause();
+		fragment.getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+		fragment = null;
 	}
 	
 	public void testAlert(View view) {
@@ -70,5 +70,22 @@ public class MainActivity extends Activity {
 		Editor editor = prefs.edit();
 		editor.putBoolean("enabled", on);
 		editor.commit();	
+	}
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+			String key) {
+		if (key.equals("pref_enabled")) {
+			Preference pref = fragment.findPreference(key);
+			boolean on = sharedPreferences.getBoolean("pref_enabled", false);
+		    if (on) {
+		    	pref.setSummary(getString(R.string.pref_enabled));
+		    	enableAlerts();
+		    } else {
+		    	pref.setSummary(getString(R.string.pref_disabled));
+		    	disableAlerts();
+		    }
+		}
+		
 	}
 }
