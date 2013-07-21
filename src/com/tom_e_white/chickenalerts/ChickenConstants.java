be92ccedf0bfbf54aea02446000bfb5fbb7b033b
Tom White <tom.e.white@gmail.com>
2013-07-21 15:50:45 +0100

    Add missing file

---
+++ src/com/tom_e_white/chickenalerts/ChickenConstants.java
@@ -0,0 +1,13 @@
+package com.tom_e_white.chickenalerts;
+
+public class ChickenConstants {
+	
+	public static final String TAG = "ChickenAlerts";
+
+	public static final String PREF_ENABLED = "pref_enabled";
+	public static final String PREF_DELAY = "pref_delay";
+	public static final String PREF_NEXT_ALERT = "pref_next_alert";
+	
+	public static final int DEFAULT_DELAY = 45;
+
+}
